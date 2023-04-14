dfed516b562c8db95f1f6b1a116f4ae747366def
Jan Malek <jmalek@antmicro.com>
2023-04-14 13:03:43 +0200

    Initial Windows support

--- .github/scripts/build_verilator.sh
+++ .github/scripts/build_verilator.sh
@@ -10,6 +10,12 @@ fi
 git clone https://github.com/verilator/verilator
 pushd verilator
 
+if [ "$RUNNER_OS" = "Windows" ]; then  # MSYS2
+    pacman --noconfirm -S --needed autoconf bison flex python3 mingw-w64-x86_64-ccache mingw-w64-x86_64-autotools gperf
+    # `CPPFLAGS=-I/usr/include` doesn't work
+    cp /usr/include/FlexLexer.h include/
+fi
+
 if [ "$RUNNER_OS" = "Linux" ]; then  # Ubuntu
     sudo apt update
     sudo apt install -y git cmake ninja-build gperf ccache dfu-util device-tree-compiler wget python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file make gcc gcc-multilib g++-multilib libsdl2-dev libmagic1 autoconf flex bison perl perl-doc numactl libfl2 libfl-dev verilator help2man
--- .github/scripts/download_renode.sh
+++ .github/scripts/download_renode.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -eu
-wget --progress=dot:giga https://dl.antmicro.com/projects/renode/builds/custom/renode-1.13.2+20230411git4d56db3f.linux-portable.tar.gz
 
-mkdir -p renode
-tar xf renode-1.13.2+20230411git4d56db3f.linux-portable.tar.gz --strip-components 1 -C renode
+if [ "$RUNNER_OS" = "Linux" ]; then  # Ubuntu
+    RENODE_ARCHIVE=renode-1.13.2+20230411git4d56db3f.linux-portable.tar.gz
+    wget --progress=dot:giga "https://dl.antmicro.com/projects/renode/builds/custom/$RENODE_ARCHIVE"
+    mkdir -p renode
+    tar xf "$RENODE_ARCHIVE" --strip-components 1 -C renode
+fi
 
-rm renode-1.13.2+20230411git4d56db3f.linux-portable.tar.gz
+if [ "$RUNNER_OS" = "Windows" ]; then  # MSYS2
+    RENODE_ARCHIVE=renode_1.13.2+20230412git4d56db3f.zip
+    wget --progress=dot:giga "https://dl.antmicro.com/projects/renode/builds/custom/$RENODE_ARCHIVE"
+    unzip "$RENODE_ARCHIVE"
+    mv "$RENODE_ARCHIVE" renode
+fi
+
+rm $RENODE_ARCHIVE
 
 pip install -r renode/tests/requirements.txt
