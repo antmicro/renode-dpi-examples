2377484cbd0ecf5f5cb69a45b66ad61d3c3a56d3
Jan Olencki <jolencki@antmicro.com>
2023-06-12 13:36:52 +0200

    Refactor DPI integration and AXI RAM

--- samples/axi_ram/sim.sv
+++ samples/axi_ram/sim.sv
@@ -1,71 +1,82 @@
+//
+// Copyright (c) 2010-2023 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+
 `timescale 1ns / 1ps
 
-`include "renode_axi.sv"
-
 module sim;
-  parameter int ClockPeriod = 10;
+  parameter int ClockPeriod = 100;
   parameter int ReceiverPort = 0;
   parameter int SenderPort = 0;
   parameter string Address = "";
 
-  logic clk = 0;
+  logic clk = 1;
 
-  axi_if bus (clk);
+  renode renode (
+      .clk(clk),
+      .interrupts('0)
+  );
 
-  static renode::connection connection = new();
-  static renode::message_t  message;
+  renode_axi_if #(.AddressWidth(20)) axi (clk);
+  renode_axi_manager renode_axi_manager (
+      .bus(axi),
+      .connection(renode.bus_controller)
+  );
 
   initial begin
-    if (Address != "") connection.connect(ReceiverPort, SenderPort, Address);
-    bus.reset();
+    if (Address != "") renode.connection.connect(ReceiverPort, SenderPort, Address);
+    renode.reset();
   end
 
   always @(posedge clk) begin
     // The receive method blocks execution of the simulation.
     // It waits until receive a message from Renode.
-    if (!connection.receive(message)) $finish;
-    else bus.handle_request(connection, message);
+    renode.receive_and_handle_message();
+    if (!renode.connection.is_connected()) $finish;
   end
 
   always #(ClockPeriod / 2) clk = ~clk;
 
   axi_ram dut (
       .clk(clk),
-      .rst(~bus.areset_n),
-      .s_axi_awid(bus.awid),
-      .s_axi_awaddr(bus.awaddr),
-      .s_axi_awlen(bus.awlen),
-      .s_axi_awsize(bus.awsize),
-      .s_axi_awburst(bus.awburst),
-      .s_axi_awlock(bus.awlock),
-      .s_axi_awcache(bus.awcache),
-      .s_axi_awprot(bus.awprot),
-      .s_axi_awvalid(bus.awvalid),
-      .s_axi_awready(bus.awready),
-      .s_axi_wdata(bus.wdata),
-      .s_axi_wstrb(bus.wstrb),
-      .s_axi_wlast(bus.wlast),
-      .s_axi_wvalid(bus.wvalid),
-      .s_axi_wready(bus.wready),
-      .s_axi_bid(bus.bid),
-      .s_axi_bresp(bus.bresp),
-      .s_axi_bvalid(bus.bvalid),
-      .s_axi_bready(bus.bready),
-      .s_axi_arid(bus.arid),
-      .s_axi_araddr(bus.araddr),
-      .s_axi_arlen(bus.arlen),
-      .s_axi_arsize(bus.arsize),
-      .s_axi_arburst(bus.arburst),
-      .s_axi_arlock(bus.arlock),
-      .s_axi_arcache(bus.arcache),
-      .s_axi_arprot(bus.arprot),
-      .s_axi_arvalid(bus.arvalid),
-      .s_axi_arready(bus.arready),
-      .s_axi_rid(bus.rid),
-      .s_axi_rdata(bus.rdata),
-      .s_axi_rresp(bus.rresp),
-      .s_axi_rlast(bus.rlast),
-      .s_axi_rvalid(bus.rvalid),
-      .s_axi_rready(bus.rready)
+      .rst(~axi.areset_n),
+      .s_axi_awid(axi.awid),
+      .s_axi_awaddr(axi.awaddr),
+      .s_axi_awlen(axi.awlen),
+      .s_axi_awsize(axi.awsize),
+      .s_axi_awburst(axi.awburst),
+      .s_axi_awlock(axi.awlock),
+      .s_axi_awcache(axi.awcache),
+      .s_axi_awprot(axi.awprot),
+      .s_axi_awvalid(axi.awvalid),
+      .s_axi_awready(axi.awready),
+      .s_axi_wdata(axi.wdata),
+      .s_axi_wstrb(axi.wstrb),
+      .s_axi_wlast(axi.wlast),
+      .s_axi_wvalid(axi.wvalid),
+      .s_axi_wready(axi.wready),
+      .s_axi_bid(axi.bid),
+      .s_axi_bresp(axi.bresp),
+      .s_axi_bvalid(axi.bvalid),
+      .s_axi_bready(axi.bready),
+      .s_axi_arid(axi.arid),
+      .s_axi_araddr(axi.araddr),
+      .s_axi_arlen(axi.arlen),
+      .s_axi_arsize(axi.arsize),
+      .s_axi_arburst(axi.arburst),
+      .s_axi_arlock(axi.arlock),
+      .s_axi_arcache(axi.arcache),
+      .s_axi_arprot(axi.arprot),
+      .s_axi_arvalid(axi.arvalid),
+      .s_axi_arready(axi.arready),
+      .s_axi_rid(axi.rid),
+      .s_axi_rdata(axi.rdata),
+      .s_axi_rresp(axi.rresp),
+      .s_axi_rlast(axi.rlast),
+      .s_axi_rvalid(axi.rvalid),
+      .s_axi_rready(axi.rready)
   );
 endmodule
