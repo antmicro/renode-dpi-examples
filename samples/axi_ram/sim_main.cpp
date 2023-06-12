ac643b7c75f6f1b772616ec0c0638331605b23f2
Jan Olencki <jolencki@antmicro.com>
2023-06-12 13:36:52 +0200

    Refactor DPI integration and AXI RAM

--- samples/axi_ram/sim_main.cpp
+++ samples/axi_ram/sim_main.cpp
@@ -1,12 +1,16 @@
 //
-// Copyright (c) 2010-2021 Antmicro
+// Copyright (c) 2010-2023 Antmicro
 //
 //  This file is licensed under the MIT License.
 //  Full license text is available in 'LICENSE' file.
 //
 #include <verilated.h>
 #include "Vsim.h"
+
+#if VM_TRACE
 #include <verilated_vcd_c.h>
+#endif
+
 #include "src/renode_dpi.h"
 
 int main(int argc, char **argv, char **env)
@@ -23,20 +27,27 @@ int main(int argc, char **argv, char **env)
     contextp->commandArgs(argc, argv);
     Vsim *top = new Vsim{contextp};
 
+#if VM_TRACE
     Verilated::traceEverOn(true);
     VerilatedVcdC *tfp = new VerilatedVcdC;
     top->trace(tfp, 99);
     tfp->open("sim.vcd");
+#endif
 
     while (!contextp->gotFinish())
     {
         top->eval();
+#if VM_TRACE
         tfp->dump(contextp->time());
+#endif
         if (!top->eventsPending()) break;
         contextp->time(top->nextTimeSlot());
     }
 
+#if VM_TRACE
     tfp->close();
+#endif
+
     delete top;
     delete contextp;
     return 0;
