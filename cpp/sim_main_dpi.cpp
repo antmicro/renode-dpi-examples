b315ba6e87c5b95ddc0213c708b7e139c599b89c
Jan Olencki <jolencki@antmicro.com>
2023-08-28 18:47:55 +0200

    Create the common sim_main.cpp file

--- cpp/sim_main_dpi.cpp
+++ cpp/sim_main_dpi.cpp
@@ -1,9 +1,18 @@
 //
-// Copyright (c) 2010-2023 Antmicro
+//  Copyright 2023 Antmicro
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
-//  This file is licensed under the MIT License.
-//  Full license text is available in 'LICENSE' file.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
 #include <verilated.h>
 #include "Vsim.h"
 
@@ -47,7 +56,6 @@ int main(int argc, char **argv, char **env)
 #if VM_TRACE
     tfp->close();
 #endif
-
     delete top;
     delete contextp;
     return 0;
